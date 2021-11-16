9b666ced64fa31e1e97971cc71a247afdc8224e4
bytesparallel <jason.ross841@gmail.com>
2021-11-16 14:34:29 -0600

    committing more documentation/notes

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -7,11 +7,18 @@ import { useEffect, useState } from "react"
 export const Repairs = () => {
     /* assignCustomers holds an array of the assigned customers a dependancy/subset of customers. i.e. this is your state variable */
     const [customers, assignCustomers] = useState([])
-
+/* Once the customers array is retrieved, it needs to end up inside of the state variable "customers" */
+/* assignCustomers must be invoked to set the value of customers */
+/* customerArray => {
+    customers = customerArray
+}
+This is not allowed in React, even if it works in vanilla JS. */
     useEffect(
         () => {
             fetch("http://localhost:8088/customers")
+            /* updates application state from permanent state and converts the result to JSON */
                 .then(res => res.json())
+                /* passes the customers state variable to the .then function as a parameter and then calls that parameter in the assignCustomers function to change application state */
                 .then((customers) => { 
                     assignCustomers(customers) }
                 )
@@ -24,6 +31,7 @@ export const Repairs = () => {
         <h1>Honey Rae's Repair Shop</h1>
 
         {
+            /* .map converting items to convert items from an array to HTML */
             customers.map(
                 (customerObject) => { 
                     return <h2>(customerObject.name)</h2>
