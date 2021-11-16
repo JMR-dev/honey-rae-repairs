80648de7b77627138998ab56292508c9234db5fd
bytesparallel <jason.ross841@gmail.com>
2021-11-16 14:15:22 -0600

    committing comments/documentation on the code

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,19 +1,24 @@
+/* imports React library */
 import React from "react"
+import { useEffect, useState } from "react"
 
+/* State is stored directly in the application file */
+/* const [customers, assignCustomers] is called a deconstruction array */
 export const Repairs = () => {
+    /* assignCustomers holds an array of the assigned customers a dependancy/subset of customers. i.e. this is your state variable */
     const [customers, assignCustomers] = useState([])
 
     useEffect(
         () => {
             fetch("http://localhost:8088/customers")
                 .then(res => res.json())
-                .then(customerAr(customers) customerArray: any
-                    setCustomers(customers) => { }
+                .then((customers) => { 
+                    assignCustomers(customers) }
                 )
-        },
+    },
         []
     )
-
+/* JSX, returns whatever HTML code you want rendered to the DOM */
     return (
         <>
         <h1>Honey Rae's Repair Shop</h1>
