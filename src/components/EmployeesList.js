db68004cf062e0a046f55e093e8edf95d33224cb
bytesparallel <jason.ross841@gmail.com>
2021-11-18 11:33:44 -0600

    saving progress on chapter 6

--- src/components/EmployeesList.js
+++ src/components/EmployeesList.js
@@ -1,33 +1,39 @@
-import React from "react"
-import {
-    useEffect,
-    useState
-} from "react"
+import React, { useEffect, useState } from "react"
 
 export const EmployeeList = () => {
-    const [employees, setEmployees] = useState([])
+    const [employees, changeEmployee] = useState([])
 
     useEffect(
-            () => {
-                
-                    fetch("http://localhost:8088/employees")
-                        /* updates application state from permanent state and converts the result to JSON */
-                        .then(res => res.json())
-                        /* passes the employees state variable to the .then function as a parameter and then calls that parameter in the assignCustomers function to change application state */
-                        .then((employees) => {
-                            setEmployees(employees)
-                        })
-                }, 
-                []
-            )
-                return(
-                <> 
-                {
-            /* .map converting items to convert items from an array to HTML */
-            employees.map(
-                (employeeObject) => {
-                    return <div > <p key = {`customer--${employeeObject.id}`} > {employeeObject.name} </p></div >
-                }
-            )
-        } </> )
+        () => {
+            fetch("http://localhost:8088/employees")
+                .then(res => res.json())
+                .then((data) => {
+                    changeEmployee(data)
+                })
+        },
+        []
+    )
+
+    useEffect(() => {
+        /*
+            1. Use .map() to get the specialty of each employee
+            2. Then update a state variable to be a comma-separated string
+                (e.g. "iPhone, Printers, ...")
+        */
+    }, [employees])
+
+    return (
+        <>
+            <div>
+                Specialties:
+            </div>
+            {
+                employees.map(
+                    (employee) => {
+                        return <p key={`employee--${employee.id}`}>{employee.name}</p>
+                    }
+                )
+            }
+        </>
+    )
 }
