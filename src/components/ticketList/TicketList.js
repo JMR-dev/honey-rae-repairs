a3270a8941d2732d67246498ab78668508ac2786
bytesparallel <jason.ross841@gmail.com>
2021-11-19 10:25:33 -0600

    added ticketList barebones

---
+++ src/components/ticketList/TicketList.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react"
+
+export const TicketList = () => {
+    const [tickets, setTicketList] = useState([])
+
+    useEffect(
+        () => {
+
+        },
+        []
+    )
+
+    return (
+        <>
+
+        </>
+    )
+}
