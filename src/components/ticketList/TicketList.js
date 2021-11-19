2d20a2182b00eb6b45b2e02a2f1be309f02bc88e
bytesparallel <jason.ross841@gmail.com>
2021-11-19 10:53:55 -0600

    completed draft code on ticketList

--- src/components/ticketList/TicketList.js
+++ src/components/ticketList/TicketList.js
@@ -1,17 +1,48 @@
-import React, { useEffect, useState } from "react"
+import React, {
+    useEffect,
+    useState
+} from "react"
 
 export const TicketList = () => {
     const [tickets, setTicketList] = useState([])
+    const [totalTicketMessage, updateTotalTicketMessage] = useState("")
 
     useEffect(
         () => {
+            fetch("http:localhost:8088/serviceTickets")
+                .then(res => res.json())
+                .then((data) => {
+                    setTicketList(data)
+                })
 
         },
         []
     )
 
-    return (
-        <>
+    useEffect(
+        () => {
+            updateTotalTicketMessage(`You have ${tickets.length} customers.`)
+        },
+        [tickets]
+    )
+
+    return ( 
+    <>
+        <
+        div > {
+            totalTicketMessage
+        } </div> {
+        tickets.map(
+            (ticketObject) => {
+                return <p key = {`ticket--${ticketObject.id}`}>
+                    <ul > {ticketObject.description} 
+                </ul> </p>
+                }
+        )
+        
+    }
+
+
 
         </>
     )
